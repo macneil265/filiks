aa22edfc239697ddf6f779aee5433e07cf086ddf
Mackiee265 <macneilmponda@gmail.com>
2026-07-02 16:05:59 +0200

    04: server, shared and database packages fix

--- packages/server/src/routes/sessions.ts
+++ packages/server/src/routes/sessions.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 // import { HTTPException } from "hono/http-exception";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
-import { db } from "@filiks/database";
+import { db } from "@filiks/database/client";
 import { Role, Mode, MessageStatus } from "@filiks/database/enums";
 import { findSupportedChatModel } from "@filiks/shared";
 
